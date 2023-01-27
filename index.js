21d7977ad0216ceac0a48de080ec9b3496419c2e
Anna Barton <anna.barton327@gmail.com>
2023-01-26 17:25:23 -0700

    commit tree 0f7885aedfdf2d5bd57e6c37dcf94d99e4be3618

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.style.height = "300px";
+element.style.backgroundColor = "#27647B";
+'#27647B'
+element.textContent = "You've changed what's on the screen!";
+
+"You've changed what's on the screen!"
+element.style.fontSize = "24px";
+element.style.marginLeft = "30px";
+element.style.lineHeight = 2;
+2
+element.className = "pet-listing dog";
+
+'pet-listing dog'
+
